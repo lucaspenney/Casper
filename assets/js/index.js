cad576e1ed72c1704347b67a988f5644f9f8a25a
lucas <lucaspenney@gmail.com>
2015-10-07 22:28:46 -0700

    A couple tweaks for using casper theme in an iframe (some further simplifications to theme)

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,37 +3,39 @@
  */
 
 /* globals jQuery, document */
-(function ($, undefined) {
+(function($, undefined) {
     "use strict";
 
     var $document = $(document);
 
-    $document.ready(function () {
+    $document.ready(function() {
 
         var $postContent = $(".post-content");
         $postContent.fitVids();
 
         $(".scroll-down").arctic_scroll();
 
+        /*
         $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
             e.preventDefault();
             $("body").toggleClass("nav-opened nav-closed");
         });
+        */
 
     });
 
     // Arctic Scroll by Paul Adam Davis
     // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
+    $.fn.arctic_scroll = function(options) {
 
         var defaults = {
-            elem: $(this),
-            speed: 500
-        },
+                elem: $(this),
+                speed: 500
+            },
 
-        allOptions = $.extend(defaults, options);
+            allOptions = $.extend(defaults, options);
 
-        allOptions.elem.click(function (event) {
+        allOptions.elem.click(function(event) {
             event.preventDefault();
             var $this = $(this),
                 $htmlBody = $('html, body'),
@@ -43,12 +45,18 @@
 
             if (offset) {
                 toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top + toMove)
+                }, allOptions.speed);
             } else if (position) {
                 toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: toMove
+                }, allOptions.speed);
             } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
+                $htmlBody.stop(true, false).animate({
+                    scrollTop: ($(this.hash).offset().top)
+                }, allOptions.speed);
             }
         });
 
